679a36ff45735cb819fbf01d06ecd45eb29c4bcc
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-06 13:39:41 +0530

    first commit

---
+++ public/content/Scripts/app/Student.js
@@ -0,0 +1,55 @@
+﻿
+app = angular.module("mainapp",[]);
+
+app.controller("student-controller", function ($scope, $http) {
+    $http.get("/getAllStudents").then(function (response, status) {
+        //alert(response.data);
+        alert(response);
+        $scope.students = response.data;
+        
+        //alert(data);
+    }, function (error,status) {
+
+        alert(error.message);
+    });
+    $scope.EditRecord = function (_item) {
+        $scope.item = _item;
+        $scope.isEditMode = true;
+        $scope.isViewMode = false;
+        $scope.isAddMode = false;
+
+    }
+    $scope.DetailsRecord = function (_item) {
+        $scope.item = _item;
+        $scope.isViewMode = true;
+        $scope.isAddMode = false;
+        $scope.isEditMode = false;
+    }
+    $scope.AddRecord = function () {
+        $scope.isViewMode = false;
+        $scope.isAddMode = true;
+        $scope.isEditMode = false;
+        $scope.newitm = { id:"0",name: "", age: "", address: "" };
+        //$scope.students.push(itm);
+    }
+    $scope.SaveItem = function () {
+        if($scope.isAddMode)
+            data = $scope.newitm
+        else
+            data = $scope.item;
+        //jdata = JSON.stringify({ name: data.name, age: data.age, address: data.address });
+        $http.post("/api/StudentApi", data).then(
+            function (resp) {
+                //if ($scope.isAddMode)
+                //    alert("Record Added");
+                //else
+                //    alert("Record Modified");
+                toastr["success"]("saved success");
+                $scope.students = resp.data;
+
+            },
+            function (error) {
+                alert(error.message);
+            });
+    }
+})
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express();
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(3000);
+
+console.log("server started at port 3000...")
+app.get('/getAllStudents',function(req,res){
+console.log("getAllStudents called");
+students = [{"name":"ahmed","age":"22","address":"miraroad"},
+{"name":"raees","age":"62","address":"miraroad"},
+{"name":"viraj","age":"32","address":"miraroad"}];
+
+	res.send(students);
+});
+
+
