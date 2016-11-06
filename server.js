aa53da6b612360cea9640616c5a50db8ae443c42
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-06 13:40:54 +0530

    removed log entry

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen(3000);
 
 console.log("server started at port 3000...")
 app.get('/getAllStudents',function(req,res){
-console.log("getAllStudents called");
+
 students = [{"name":"ahmed","age":"22","address":"miraroad"},
 {"name":"raees","age":"62","address":"miraroad"},
 {"name":"viraj","age":"32","address":"miraroad"}];
