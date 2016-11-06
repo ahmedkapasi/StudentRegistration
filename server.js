e91c18758b51efadbbf2cfb0f00effadd9e618ab
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-06 22:08:31 +0530

    made heroku related changes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 
 console.log("server started at port 3000...")
 student = {};
@@ -12,12 +13,9 @@ app.get('/getAllStudents',function(req,res){
 students = [{"id":"1","name":"ahmed","age":"22","address":"miraroad"},
 {"id":"2","name":"raees","age":"62","address":"miraroad"},
 {"id":"3","name":"viraj","age":"32","address":"miraroad"}];
-
 	res.send(students);
 });
-
 app.post("/SaveStudentRecord",function(req,res){
 	console.log(req.body);
-	//students.push(req.body);
 	res.send(students);
 });
