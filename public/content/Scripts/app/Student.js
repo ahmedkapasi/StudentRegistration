cf26470913c584047129442c33dd4a5f66a5ad3e
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-06 21:15:02 +0530

    added few file

--- public/content/Scripts/app/Student.js
+++ public/content/Scripts/app/Student.js
@@ -38,12 +38,9 @@ app.controller("student-controller", function ($scope, $http) {
         else
             data = $scope.item;
         //jdata = JSON.stringify({ name: data.name, age: data.age, address: data.address });
-        $http.post("/api/StudentApi", data).then(
+        $http.post("/SaveStudentRecord", data).then(
             function (resp) {
-                //if ($scope.isAddMode)
-                //    alert("Record Added");
-                //else
-                //    alert("Record Modified");
+                
                 toastr["success"]("saved success");
                 $scope.students = resp.data;
 
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express')
 var app = express();
-
+var bodyParser = require('body-parser');
 app.use(express.static(__dirname+'/public'));
-
+app.use(bodyParser.json());
 app.listen(3000);
 
 console.log("server started at port 3000...")
+student = {};
 app.get('/getAllStudents',function(req,res){
 
-students = [{"name":"ahmed","age":"22","address":"miraroad"},
-{"name":"raees","age":"62","address":"miraroad"},
-{"name":"viraj","age":"32","address":"miraroad"}];
+students = [{"id":"1","name":"ahmed","age":"22","address":"miraroad"},
+{"id":"2","name":"raees","age":"62","address":"miraroad"},
+{"id":"3","name":"viraj","age":"32","address":"miraroad"}];
 
 	res.send(students);
 });
 
-
+app.post("/SaveStudentRecord",function(req,res){
+	console.log(req.body);
+	//students.push(req.body);
+	res.send(students);
+});
