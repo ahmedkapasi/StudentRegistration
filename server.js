a6af2037b4006f6769757cbdde81d5bf3c242d62
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-07 17:04:35 +0530

    added / in the path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express();
 
 var bodyParser = require('body-parser');
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/public/'));
 app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 app.listen(port);
