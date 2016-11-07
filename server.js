2416a74b324e09d17a1d97e4b586ee75a2e996b8
ahmedkapasi <ahmed.kapasi@gmail.com>
2016-11-07 17:41:52 +0530

    added routing logic

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.listen(port);
 console.log("server started at port "+ port);
 student = {};
 app.get('/',function(req,res){
-	res.redirect("index.html");
+	res.render("index.html");
 })
 app.get('/getAllStudents',function(req,res){
 
